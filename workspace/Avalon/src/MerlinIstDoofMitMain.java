9dba991a6339d964c836c36946e239879f4be710
drachenbach <martinachenbach93@gmail.com>
2013-11-25 21:49:46 +0100

    yeees

--- workspace/Avalon/src/MerlinIstDoofMitMain.java
+++ workspace/Avalon/src/MerlinIstDoofMitMain.java
@@ -1,12 +1,9 @@
 public class MerlinIstDoofMitMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public static void main(String[] args) {
+	for (int i = 0; i < 100; i++) {
+		System.out.println("Wir sind die BESTEN! Sogar besser als Olli!");
 	}
-
 }
 
-public class test {
-	
 }
