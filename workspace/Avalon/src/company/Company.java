a9a99084f93efc5785e1e52dbd28585750aa7911
jayjay04 <jonas.braun@live.de>
2013-11-26 13:27:48 +0100

    neue functions

--- workspace/Avalon/src/company/Company.java
+++ workspace/Avalon/src/company/Company.java
@@ -1,6 +1,47 @@
 package company;
 
 public class Company {
+	private String playername;
+	private int popularity;
+	private int money;
 
-	// hallo
+// money functions	
+public int getMoney() {
+	return this.money;
 }
+public void addMoney(int value){
+	this.money = this.money + value;
+}
+public void reduceMoney(int value){
+	this.money = this.money - value;
+}
+
+// popularity functions
+public int getPopularity() {
+	return this.popularity;
+}
+public void addPopularity(int value){
+	this.popularity = this.popularity + value;
+}
+public void reducePopularity(int value){
+	this.popularity = this.popularity - value;
+}
+
+// simulation functions
+ public void simActivities(){
+	 
+ }
+ 
+ 
+public void initialize(){
+	private Marketing  marketing;
+	private Research research;
+	private Storage storage;
+	private LegalDepartment legaldepartment;
+	private Purchase purchase;
+	
+	
+	}
+
+}
+
