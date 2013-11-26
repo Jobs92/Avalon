408d3c2cf2f4c19bbfe785512a2965ef5817019a
Jobs92 <johannes.haass@gmail.com>
2013-11-26 13:06:33 +0100

    Sales, Product, Warehouse
    3 new classes, 1 new package

---
+++ workspace/Avalon/src/Product/Product.java
@@ -0,0 +1,36 @@
+package Product;
+
+
+public class Product {
+	
+	private int level;
+	private int price;
+	private int amount;
+	private int quality;
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getQuality() {
+		return quality;
+	}
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+	
+
+}
---
+++ workspace/Avalon/src/departments/Warehouse.java
@@ -0,0 +1,5 @@
+package departments;
+
+public class Warehouse {
+
+}
