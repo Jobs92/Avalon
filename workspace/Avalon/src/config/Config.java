3d650345d4bb04377ca8cf8e77ac3636d69535c4
jayjay04 <jonas.braun@live.de>
2013-12-02 13:08:12 +0100

    changes config

--- workspace/Avalon/src/config/Config.java
+++ workspace/Avalon/src/config/Config.java
@@ -2,9 +2,21 @@ package config;
 
 public class Config {
 	private int maxLevelResearch;
+	private int[] supplierTrust = new int[3];
+	private int[] supplierQuality = new int[3];
+	
+	public int[] getSupplierTrust() {
+		return supplierTrust;
+	}
+
+	public int[] getSupplierQuality() {
+		return supplierQuality;
+	}
 
 	public int getMaxLevelResearch() {
 		return maxLevelResearch;
 	}
+	
+	
 
 }
