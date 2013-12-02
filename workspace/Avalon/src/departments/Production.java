0f918ce239287aa542b83d4fde079639223c971c
Jobs92 <johannes.haass@gmail.com>
2013-12-02 12:52:50 +0100

    Production changes

--- workspace/Avalon/src/departments/Production.java
+++ workspace/Avalon/src/departments/Production.java
@@ -12,10 +12,16 @@ public class Production extends Department {
 	public void setLevel(int level) {
 		this.level = level;
 	}
+	public void addLevel(int level) {
+		this.level += level;
+	}
 	public int getCapacity() {
 		return capacity;
 	}
 	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public void addCapacity(int capacity) {
 		this.capacity = capacity;
 	}
 	@Override
@@ -23,5 +29,20 @@ public class Production extends Department {
 		// TODO Auto-generated method stub
 		
 	}
+	public void produve (int level, int amount){ 
+		int targat_amount=company.getWarehouse().getSingleProduct(level).getAmount();    //Helper
+		int target_price=company.getWarehouse().getSingleProduct(level).getPrice();
+		
+		
+		
+		if (capacity>amount) {
+			 // ToDo: Error Massaage oder so?
+		}
+		else {
+			
+			company.getWarehouse().getSingleProduct(level).setAmount(targat_amount-amount);
+			this.revenue+=amount*target_price;
+		}
+		
 	
 }
