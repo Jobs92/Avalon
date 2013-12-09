87fd7e8c58dbcdd5d8b04662ea147ac8af94dcc5
Fred93 <frederik.pahde@gmx.de>
2013-12-09 19:11:05 +0100

    setter und getter

--- workspace/Avalon/src/market/ConsumerGroup.java
+++ workspace/Avalon/src/market/ConsumerGroup.java
@@ -13,6 +13,47 @@ public class ConsumerGroup {
 		this.percentage	= percentage;
 	}
 	
+	
+	public int getPopularity() {
+		return popularity;
+	}
+
+
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public int getPercentage() {
+		return percentage;
+	}
+
+
+	public void setPercentage(int percentage) {
+		this.percentage = percentage;
+	}
+
+
 	public void simulate(){
 		
 	}
