22b4562401deadaf9761d90bdb95955f22d5dde1
Fred93 <frederik.pahde@gmx.de>
2013-12-09 18:59:37 +0100

    consumer group

---
+++ workspace/Avalon/src/market/ConsumerGroup.java
@@ -0,0 +1,20 @@
+package market;
+
+public class ConsumerGroup {
+	private int popularity;
+	private int price;
+	private int level;
+	private int percentage;
+	
+	public ConsumerGroup(int popularity, int price, int level, int percentage){
+		this.popularity = popularity;
+		this.price 		= price;
+		this.level		= level;
+		this.percentage	= percentage;
+	}
+	
+	public void simulate(){
+		
+	}
+	
+}
--- workspace/Avalon/src/market/ConsumerGroups.java
+++
@@ -1,5 +0,0 @@
-package market;
-
-public class ConsumerGroups {
-	
-}
