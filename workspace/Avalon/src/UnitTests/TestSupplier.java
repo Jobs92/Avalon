86b6ae24152a26fa3842245a726694703b03cf8d
Fred93 <frederik.pahde@gmx.de>
2013-12-13 20:20:13 +0100

    Wrong names

--- workspace/Avalon/src/UnitTests/TestSupplier.java
+++ workspace/Avalon/src/UnitTests/TestSupplier.java
@@ -39,28 +39,22 @@ public class TestSupplier {
 		gameManager.addPlayer(company2);
 		gameManager.startGame();
 	}
-
-//	@Test
-//	public void testAddCompany() {
-//		gameManager.addPlayer(company3);
-//		assertEquals(gameManager.getPlayer().size(), 1);
-//	}
 	
-	@Test
-	public void testNextRound() {
-		assertEquals(2, GameManager.sharedInstance().getPlayer().size());
-		company1.setReady(true);
-		company2.setReady(true);
-		assertEquals(2, gameManager.getRound());
-		company1.setReady(true);
-		assertEquals(2, gameManager.getRound());
-		company2.setReady(true);
-		assertEquals(3, gameManager.getRound());
-		company1.changeMoney(100000);
-		company1.setReady(true);
-		company2.setReady(true);
-		assertEquals(false, GameManager.sharedInstance().getActive());
-		assertEquals(company1, GameManager.sharedInstance().getWinner());
+	@SuppressWarnings("deprecation")
+	@Test //supplier
+	public void testSupplier(){
+		ArrayList<Supplier> sup = Market.sharedInstance().getSupplier();
+		assertEquals(Config.getSupplierPrice()[0], sup.get(0).getPrice(), 0);
+		assertEquals(Config.getSupplierQuality()[0], sup.get(0).getQuality(), 0);
+		assertEquals(Config.getSupplierTrust()[0], sup.get(0).getTrustiness(), 0);
+		assertEquals(Config.getSupplierPrice()[1], sup.get(1).getPrice(), 0);
+		assertEquals(Config.getSupplierQuality()[1], sup.get(1).getQuality(), 0);
+		assertEquals(Config.getSupplierTrust()[1], sup.get(1).getTrustiness(), 0);
+		assertEquals(Config.getSupplierPrice()[2], sup.get(2).getPrice(), 0);
+		assertEquals(Config.getSupplierQuality()[2], sup.get(2).getQuality(), 0);
+		assertEquals(Config.getSupplierTrust()[2], sup.get(2).getTrustiness(), 0);
+	
+
 	}
 	
 	@After
@@ -68,24 +62,6 @@ public class TestSupplier {
 		GameManager.sharedInstance().deleteInstance();
 		Market.sharedInstance().deleteInstance();
 	}
-	
-	
-//	@SuppressWarnings("deprecation")
-//	@Test //supplier
-//	public void testSupplier(){
-//		ArrayList<Supplier> sup = Market.sharedInstance().getSupplier();
-//		assertEquals(Config.getSupplierPrice()[0], sup.get(0).getPrice(), 0);
-//		assertEquals(Config.getSupplierQuality()[0], sup.get(0).getQuality(), 0);
-//		assertEquals(Config.getSupplierTrust()[0], sup.get(0).getTrustiness(), 0);
-//		assertEquals(Config.getSupplierPrice()[1], sup.get(1).getPrice(), 0);
-//		assertEquals(Config.getSupplierQuality()[1], sup.get(1).getQuality(), 0);
-//		assertEquals(Config.getSupplierTrust()[1], sup.get(1).getTrustiness(), 0);
-//		assertEquals(Config.getSupplierPrice()[2], sup.get(2).getPrice(), 0);
-//		assertEquals(Config.getSupplierQuality()[2], sup.get(2).getQuality(), 0);
-//		assertEquals(Config.getSupplierTrust()[2], sup.get(2).getTrustiness(), 0);
-//	
-//
-//	}
 	
 
 }
