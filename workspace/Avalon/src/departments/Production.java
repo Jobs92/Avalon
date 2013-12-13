8b497b81ea9b379def90717cc6c92d81bcc73097
Fred93 <frederik.pahde@gmx.de>
2013-12-13 19:37:37 +0100

    removed some methods in production

--- workspace/Avalon/src/departments/Production.java
+++ workspace/Avalon/src/departments/Production.java
@@ -7,7 +7,7 @@ import config.Config;
 import product.Product;
 
 public class Production extends Department {
-	
+	private int fixcosts = Config.getProductionFixcost();
 	private int level;
 	private int capacity;
 	private ArrayList<otherclasses.Jobs> history;
@@ -17,39 +17,24 @@ public class Production extends Department {
 		history = new ArrayList<otherclasses.Jobs>();
 		level = 1;
 		capacity=config.Config.getProductionCapacity();
-		
 	}
-	
-	
-	
 	
 	public int getLevel() {
 		return level;
 	}
-	public void setLevel(int level) {
-		this.level = level;
-	}
-	public void addLevel(int level) {
-		this.level += level;
-		this.upgrade();
-	}
+	
 	public int getCapacity() {
 		return capacity;
 	}
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
-	public void addCapacity(int capacity) {
-		this.capacity = capacity;
-	}
+	
 	@Override
 	public void simulate() {
+		super.company.changeMoney((-1)*fixcost);
+		//TODO: simulate production
 		// TODO Auto-generated method stub
 		
 	}
 	public void produce (int level, int amount){ 
-
-		
 		if (capacity<amount) {
 			 // ToDo: Error Massaage oder so?
 		}
@@ -61,9 +46,6 @@ public class Production extends Department {
 			} else {
 				company.getWarehouse().getSingleProduct(level).setAmount(company.getWarehouse().getSingleProduct(level).getAmount()+amount);
 			}
-			
-			
-		
 		}
 		
 	}
@@ -74,9 +56,5 @@ public class Production extends Department {
 			super.company.changeMoney((-1)*Config.getCostsUpgradeProduction());
 			this.capacity=(int)(this.capacity*Config.getUpgradeProduction());
 		}
-	}
-
-
-
-		
+	}		
 }
