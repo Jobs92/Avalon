e31dccdba2399a2eee9bd21e79538159acc0e38c
Fred93 <frederik.pahde@gmx.de>
2013-12-31 17:07:54 +0100

    some api updates

--- workspace/Avalon/src/client/api.java
+++ workspace/Avalon/src/client/api.java
@@ -11,4 +11,84 @@ public class api {
 		String s = "PRODUCE " + x;
 		connection.send(s);
 	}
+	
+	public void ready(){
+		String s = "READY ";
+		connection.send(s);
+	}
+	
+	public void upgradeMarketing(){
+		String s = "UPGRADEMARKETING ";
+		connection.send(s);
+	}
+	
+	public void startMarketingCampaign(int campaign){
+		String s = "STARTMARKETINGCAMPAIGN " + campaign;
+		connection.send(s);
+	}
+	
+	public void buy(int supplier, int amount){
+		String s = "BUY " + supplier + "#" + amount;
+		connection.send(s);
+	}
+	
+	public void upgradeProduction(){
+		String s = "UPGRADEPRODUCTION ";
+		connection.send(s);
+	}
+	
+	public void startResearchCampaign(int campaign){
+		String s = "STARTRESEARCHCAMPAIGN " + campaign;
+		connection.send(s);
+	}
+	
+	public void upgradeResearch(){
+		String s = "UPGRADERESEARCH ";
+		connection.send(s);
+	}
+	
+	public void patent(){
+		String s = "PATENT ";
+		connection.send(s);
+	}
+	
+	public void release(){
+		String s = "RELEASE ";
+		connection.send(s);
+	}
+	
+	public void spy(int campaign, int opponent){
+		String s = "SPY " + campaign + "#" + opponent;
+		connection.send(s);
+	}
+	
+	public void setPrice(int level, int amount){
+		String s = "PRICE " + level + "#" + amount;
+		connection.send(s);
+	}
+	
+	public void checkOpponent(int opponent){
+		String s = "CHECK " + opponent;
+		connection.send(s);
+	}
+	
+	public void sueOpponent(int opponent){
+		String s = "SUE " + opponent;
+		connection.send(s);
+	}
+	
+	public void payAmount(){
+		String s = "PAYAMOUNT ";
+		connection.send(s);
+	}
+	
+	public void abandonLawsuit(){
+		String s = "ABANDONLAWSUIT ";
+		connection.send(s);
+	}
+	
+	public void upgradeLegalDepartment(){
+		String s = "UPGRADELEGALDEPARTMENT ";
+		connection.send(s);
+	}
 }
--- workspace/Avalon/src/utils/SnapshotData.java
+++ workspace/Avalon/src/utils/SnapshotData.java
@@ -4,6 +4,15 @@ import java.io.Serializable;
 
 public class SnapshotData implements Serializable{
 	private double money;
+	private double image;
+	private double fixCosts;
+	private double varCosts;
+	private String[] products;
+	private String[] supplier;
+	private int[] level; //Research#LegalDepartment#Marketing#spying#patent
+	private String[] messages;
+	private int round;
+	
 
 	public double getMoney() {
 		return money;
@@ -12,5 +21,71 @@ public class SnapshotData implements Serializable{
 	public void setMoney(double money) {
 		this.money = money;
 	}
+
+	public double getImage() {
+		return image;
+	}
+
+	public void setImage(double image) {
+		this.image = image;
+	}
+
+	public double getFixCosts() {
+		return fixCosts;
+	}
+
+	public void setFixCosts(double fixCosts) {
+		this.fixCosts = fixCosts;
+	}
+
+	public double getVarCosts() {
+		return varCosts;
+	}
+
+	public void setVarCosts(double varCosts) {
+		this.varCosts = varCosts;
+	}
+
+	public String[] getProducts() {
+		return products;
+	}
+
+	public void setProducts(String[] products) {
+		this.products = products;
+	}
+
+	public String[] getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(String[] supplier) {
+		this.supplier = supplier;
+	}
+
+	public int[] getLevel() {
+		return level;
+	}
+
+	public void setLevel(int[] level) {
+		this.level = level;
+	}
+
+	public String[] getMessages() {
+		return messages;
+	}
+
+	public void setMessages(String[] messages) {
+		this.messages = messages;
+	}
+
+	public int getRound() {
+		return round;
+	}
+
+	public void setRound(int round) {
+		this.round = round;
+	}
+	
+	
 	
 }
