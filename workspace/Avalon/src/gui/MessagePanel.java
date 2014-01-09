0cbc4f955c1596c27d9ad7f0a42b745fb09546cc
drachenbach <martinachenbach93@gmail.com>
2014-01-09 20:20:49 +0100

    GUI Messages

--- workspace/Avalon/src/gui/MessagePanel.java
+++ workspace/Avalon/src/gui/MessagePanel.java
@@ -1,7 +1,8 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.Dictionary;
 
 import javax.swing.JList;
 import javax.swing.JOptionPane;
@@ -14,8 +15,11 @@ import javax.swing.event.ListSelectionListener;
 @SuppressWarnings("serial")
 public class MessagePanel extends AvalonPanel {
 	private JList<String> inbox;
-	private Vector<String> data = new Vector<String>();
-	private Vector<Message> extendedData = new Vector<Message>();
+	private String[] titles;
+	private String[] messages;
+
+	// private Vector<String> titles = new Vector<String>();
+	// private Vector<Message> messages = new Vector<Message>();
 
 	public MessagePanel() {
 		TitledBorder tb = new TitledBorder("Message Inbox");
@@ -23,13 +27,15 @@ public class MessagePanel extends AvalonPanel {
 		setLayout(new BorderLayout());
 
 		// sample data
-		for (int i = 0; i < 100; i++) {
-			Message m = new Message(String.valueOf(i), String.valueOf(i * i));
-			extendedData.add(m);
-			data.add(m.getTitle());
+		titles = new String[19];
+		messages = new String[19];
+		for (int i = 0; i < titles.length; i++) {
+			titles[i] = "Message #" + i;
+			messages[i] = "Quadrat von " + i + "=" + i * i;
 		}
 
-		inbox = new JList<String>(data);
+		inbox = new JList<String>();
+		inbox.setListData(titles);
 		inbox.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
 		inbox.addListSelectionListener(new ListSelectionListener() {
@@ -39,9 +45,7 @@ public class MessagePanel extends AvalonPanel {
 				if (e.getValueIsAdjusting())
 					return;
 				int i = inbox.getSelectedIndex();
-				Message m = extendedData.get(i);
-				JOptionPane.showConfirmDialog(null, m.getMessage(),
-						m.getTitle(), JOptionPane.OK_OPTION,
+				JOptionPane.showMessageDialog(null, messages[i], titles[i],
 						JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
@@ -51,7 +55,14 @@ public class MessagePanel extends AvalonPanel {
 
 	@Override
 	protected void fill() {
-		String[] messages = GuiManager.sharedInstance().getDs().getMessages();
-		inbox.setListData(messages);
+		ArrayList<Dictionary<String, String>> input = GuiManager
+				.sharedInstance().getDs().getMessages();
+		titles = new String[input.size()];
+		messages = new String[input.size()];
+		for (int i = 0; i < input.size(); i++) {
+			titles[i] = input.get(i).get("title");
+			messages[i] = input.get(i).get("message");
+		}
+		inbox.setListData(titles);
 	}
 }
