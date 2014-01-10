fe56050984b4308efb39d398f12c9a374be93711
drachenbach <martinachenbach93@gmail.com>
2014-01-10 17:43:45 +0100

    added Incident class

---
+++ workspace/Avalon/src/utils/Incident.java
@@ -0,0 +1,50 @@
+package utils;
+
+public class Incident {
+	private String title;
+	private String description;
+	private int target; // if everyone: -1
+	private int probability;
+
+	public Incident(String title, String description, int target,
+			int probability) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.target = target;
+		this.probability = probability;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getTarget() {
+		return target;
+	}
+
+	public void setTarget(int target) {
+		this.target = target;
+	}
+
+	public int getProbability() {
+		return probability;
+	}
+
+	public void setProbability(int probability) {
+		this.probability = probability;
+	}
+
+}
