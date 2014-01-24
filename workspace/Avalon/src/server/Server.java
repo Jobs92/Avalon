bcc31240e098ce64aaabc84d66b06847275ce215
drachenbach <martinachenbach93@gmail.com>
2014-01-24 17:51:52 +0100

    sysos

--- workspace/Avalon/src/server/Server.java
+++ workspace/Avalon/src/server/Server.java
@@ -16,12 +16,12 @@ public class Server {
 	public static void main(String args[]) {
 		try {
 			server = new ServerSocket(56557);
-			System.out.println(server.getLocalPort());
+			System.out.println("Server started on Port "+server.getLocalPort());
 			while (true) {
 				Socket skt = server.accept();
 				Connection conn = new Connection(skt);
 				ServerMessageHandler.sharedInstance().addPlayer(conn);
-				System.out.println("Neue Vervindung");
+				System.out.println("New Connection");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
