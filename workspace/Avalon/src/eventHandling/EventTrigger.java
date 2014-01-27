698f5d0f81d2da797938bb0f00b3c7fc2f84d65b
drachenbach <martinachenbach93@gmail.com>
2014-01-27 17:37:20 +0100

    dunno

---
+++ workspace/Avalon/src/eventHandling/EventTrigger.java
@@ -0,0 +1,18 @@
+package eventHandling;
+
+public class EventTrigger {
+	private Event event;
+	private int counter;
+	
+	public EventTrigger(Event e, int c){
+		this.event = e;
+		this.counter = c;
+	}
+
+	public void simulate() {
+		if (counter==0) {
+			
+		}
+		
+	}
+}
