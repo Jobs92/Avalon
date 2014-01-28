d2abb97e34b39a6847414cfe304ac8c5fd5d3bba
jayjay04 <jonas.braun@live.de>
2014-01-28 17:28:01 +0100

    junit sales

--- workspace/Avalon/src/UnitTests/TestSales.java
+++ workspace/Avalon/src/UnitTests/TestSales.java
@@ -1,14 +1,59 @@
 package UnitTests;
 
 import static org.junit.Assert.*;
+import gameManager.GameManager;
+import market.Market;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import product.Product;
+import company.Company;
+import config.Config;
 
 public class TestSales {
 
+	
+	private Company company;
+	private GameManager gameManager;
+
+	@Before
+	public void prepareTest() {
+		gameManager = GameManager.sharedInstance();
+		company = new Company();
+	}
+
+	
+	@Before
+	public void startGame(){
+		gameManager.addPlayer(company);
+	}
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//Pay fixcosts		
+		company.getSales().payFixcosts();
+		company.getSales().simulate();
+		assertEquals(company.getMoney(), Config.getCompanyStartMoney() - Config.getSalesFixcost(), 0);
+		
+		//Create products and get amount 
+		Product product = new Product(10, "test", company);
+		Product product2 = new Product(10, "test", company);
+		company.getWarehouse().addProduct(product);
+		company.getWarehouse().addProduct(product2);
+		company.getSales().setPrice(10, 1000);
+		company.getSales().sell(10, 2); // Problem with history
+		company.getSales().simulate();
+		assertEquals(2000, company.getSales().getRevenue());
+		
+		
+	}
+	
+	@After
+	public void removeInstances(){
+		GameManager.sharedInstance().deleteInstance();
+		Market.sharedInstance().deleteInstance();
 	}
 
 }
