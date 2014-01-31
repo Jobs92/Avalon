dd2e3a9a3ff2e9212009adf7552c63299076b855
drachenbach <martinachenbach93@gmail.com>
2014-01-31 16:18:52 +0100

    new data for companypanel

--- workspace/Avalon/src/gui/CompanyPanel.java
+++ workspace/Avalon/src/gui/CompanyPanel.java
@@ -7,62 +7,37 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 
+import utils.DataSnapshot;
+
 @SuppressWarnings("serial")
 public class CompanyPanel extends AvalonPanel {
 
-	private JLabel labelMoney = new JLabel("Money");
-	private JLabel money = new JLabel("0", SwingConstants.RIGHT);
-
-	private JLabel labelFixcosts = new JLabel("Fixcosts");
-	private JLabel fixcosts = new JLabel("0", SwingConstants.RIGHT);
-
-	private JLabel labelVarCosts = new JLabel("Variable Costs");
-	private JLabel varCosts = new JLabel("0", SwingConstants.RIGHT);
-
-	private JLabel labelProductsOnStock = new JLabel("Products on Stock");
-	private JLabel productsOnStock = new JLabel("0", SwingConstants.RIGHT);
-
-	private JLabel labelProductLevel = new JLabel("Highest Product Level");
-	private JLabel productLevel = new JLabel("0", SwingConstants.RIGHT);
+	private JLabel infoLabel = new JLabel();
 
 	public CompanyPanel() {
 		TitledBorder tb = new TitledBorder("Company");
 		setBorder(tb);
-		setLayout(new GridLayout(5, 2));
-//		setBackground(new Color(255, 0, 0, 95));
-
-		addLabels();
-	}
-
-	private void addLabels() {
-		add(labelMoney);
-		add(money);
-
-		add(labelFixcosts);
-		add(fixcosts);
-
-		add(labelVarCosts);
-		add(varCosts);
-
-		add(labelProductLevel);
-		add(productLevel);
-
-		add(labelProductsOnStock);
-		add(productsOnStock);
+		// setLayout(new GridLayout(5, 2));
+		// setBackground(new Color(255, 0, 0, 95));
+		add(infoLabel);
 	}
 
 	@Override
 	protected void fill() {
-		money.setText(String.valueOf(GuiManager.sharedInstance().getDs()
-				.getMoney()));
-		fixcosts.setText(String.valueOf(GuiManager.sharedInstance().getDs()
-				.getFixCosts()));
-		varCosts.setText(String.valueOf(GuiManager.sharedInstance().getDs()
-				.getVarCosts()));
-		productLevel.setText(String.valueOf(GuiManager.sharedInstance().getDs()
-				.getHighestProductLevel()));
-		productsOnStock.setText(String.valueOf(GuiManager.sharedInstance()
-				.getDs().getProductsOnStock()));
+		DataSnapshot ds = GuiManager.sharedInstance().getDs();
+		String infoString = "<html><ul><li>Money: " + ds.getMoney() + "</li>";
+		infoString += "<li>Profit: " + ds.getProfit() + "</li>";
+		infoString += "<li>Revenue: " + ds.getRevenue() + "</li>";
+		infoString += "<li>Total Costs: " + ds.getTotalCosts() + "</li>";
+		infoString += "<li>Sold Smartphones: " + ds.getSoldSmartphones()
+				+ "</li>";
+		infoString += "<li>Fixcosts: " + ds.getFixCosts() + "</li>";
+		infoString += "<li>Highest Product Level: "
+				+ ds.getHighestProductLevel() + "</li>";
+		infoString += "<li>Products on Stock: " + ds.getProductsOnStock()
+				+ "</li>";
+		infoString += "</ul></html>";
+		infoLabel.setText(infoString);
 		refresh();
 	}
 
